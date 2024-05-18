a670e8e8ec1b87a33b685880d19bdb1a7fa7d007
Garrodes <batuque77mlh@gmail.com>
2024-05-18 10:31:50 +0200

    ADD : translation model

--- slovo/translation/models.py
+++ slovo/translation/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-class Translation(models):
-    text = models.CharField(max_length=512)
-    translation = models.CharField(max_length=512)
+class Translation(models.Model):
+
+    text = models.CharField(max_length=256)
+    translation = models.CharField(max_length=256)
+    # created_by = models.ForeignKey(User, related_name='translations')
+    created_at = models.DateTimeField(auto_now_add=True)
     
--- slovo/translation/views.py
+++ slovo/translation/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_protect
-import requests , json , os
-from requests import RequestException
 import logging
 from datetime import datetime
+from .models import Translation
 from .forms import TranslateForm, FeedbackForm
 from .translate import make_translation
 
@@ -16,6 +15,7 @@ logging.basicConfig(level=logging.INFO)
 # @login_required(login_url='/login')
 @require_http_methods(["POST","GET"])
 def translate(request):
+    print(request.user)
     if request.method == 'POST' and ('translate' in request.POST or 'feedback' in request.POST):
         if 'translate' in request.POST :
             form = TranslateForm(request.POST)
@@ -34,7 +34,10 @@ def translate(request):
             return render(request,'translation/translation.html',{'form':second_form,"translation":translation['translation']})
         elif 'feedback' in request.POST :
             form = FeedbackForm(request.POST)
-            # a partir de ce forumaire
+            if form.is_valid() :
+                form_data = form.cleaned_data 
+                translation = Translation(text=form_data['text'],translation=form_data['translation'])
+                translation.save()
             return render(request,'translation/translation.html',{'form':form})
     else:
         translation= {"translation":""}
