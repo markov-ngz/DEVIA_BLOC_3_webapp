9b53c9fd566569dbb36dbd269b2f593b7a4b2547
Garrodes <batuque77mlh@gmail.com>
2024-05-22 22:05:19 +0200

    ADD doker compose.yml

--- slovo/slovo/settings.py
+++ slovo/slovo/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =  os.getenv('DEBUG','') == 'True'
 
 
 ALLOWED_HOSTS = ['*']
@@ -128,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
