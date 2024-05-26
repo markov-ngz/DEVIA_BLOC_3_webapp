e97b8fa1d3c6d9f1df9fdef7bd10cf10b97a2574
MarkReduce <123246412+markov-ngz@users.noreply.github.com>
2024-05-26 10:20:59 +0200

    BUG : removing django browser reload

--- slovo/slovo/settings.py
+++ slovo/slovo/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
