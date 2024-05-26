abac9055535868044aec660a1587adc51e107a20
MarkReduce <123246412+markov-ngz@users.noreply.github.com>
2024-05-26 10:59:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
-python manage.py migrate --no-input
-python manage.py tailwind install --no-input;
-python manage.py tailwind build --no-input;
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --no-input
 
 gunicorn slovo.wsgi:application --bind 0.0.0.0:8000
